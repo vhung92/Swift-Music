12ccfa8300b3d4e43e5dfbe439f70352f91fdc9f
Daniel Schlaug <daniel.schlaug@gmail.com>
2014-10-22 18:55:02 +0200

    Bloody GitHub not accepting my nice grand piano

--- SwiftMusic/SwiftMusic/ViewController.swift
+++ SwiftMusic/SwiftMusic/ViewController.swift
@@ -9,9 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
+    let midiView = EndlessMIDIView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        midiView.start()
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, Int64(UInt64(1) * NSEC_PER_SEC)), dispatch_get_main_queue()) {
+                self.midiView.startNote(60, withVelocity: 60, onChannel: 1)
+        }
         // Do any additional setup after loading the view, typically from a nib.
     }
 
